4fb1d372a01e90b5a893388d09a4e51e3e5ef694
Bill Derouin <derouinbill@gmail.com>
2014-07-24 20:54:07 +0000

    Added tictactoe

--- tictactoe.py
+++ tictactoe.py
@@ -1,56 +1,22 @@
 from random import randint
 
 def printBoard():
-	print board[0] + " | " + board[1] + " | " + board[2]
-	print "---------"
-	print board[3] + " | " + board[4] + " | " + board[5]
-	print "---------"
-	print board[6] + " | " + board[7] + " | " + board[8]
+	print " " + board[0] + " | " + board[1] + " | " + board[2]
+	print "-----------"
+	print " " + board[3] + " | " + board[4] + " | " + board[5]
+	print "-----------"
+	print " " + board[6] + " | " + board[7] + " | " + board[8]
 
-def checkWin(cur):
-	# cur is the last move, that way only check moves related
-	return False
-
-def checkLoss(cur):
-	return False
-
-board = []
-
-for i in range(9):
+board =[]
+for i in range (9):
 	board.append(str(i))
 
-playing = True
-
-while playing:
+print "wecome to tictactoe"
+while True:
 	printBoard()
-	input = int(raw_input("Choose an option (-1 to quit): "))
-	if input == -1:
-		playing = False
-		break
-	if input < 0 or input > 8:
-		print "Not a valid option"
+	print "pick one number on the board "
+	input = int(raw_input())
+	if input > 8 or input < 0:
+		print "you can't pick a number lower than 0 or higher then 8"
 		continue
-	if board[input] == "X" or board[input] == "O":
-		print "That spot has been taken"
-		continue
-	board[input] = "X"
-
-	if checkWin(input):
-		playing = False
-		print "You win!"
-		break
-
-	valid = []
-	for i in range(9):
-		if board[i] != "X" and board[i] != "O":
-			valid.append(i)
-
-	num = valid[randint(0, len(valid)-1)]
-	board[num] = "O"
-
-	if checkLoss(num):
-		playing = False
-		print "You lose"
-		break
-
-print "Thanks for playing!"
+	
