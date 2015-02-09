d9f7aa3f4958a214fa1d973a3e4cec2e495f6b9b
Sam Green <samantha.green03@gmail.com>
2015-02-08 16:18:44 -0800

    homework 1 finished

--- homework/hw1.py
+++ homework/hw1.py
@@ -1,11 +1,16 @@
 # print out some basic information about yourself
 
 # name
+print "Samantha"
 
 # age
+print"11"
 
 # school
+print"eagle rock elementry"
 
 # favorite food
+print"ice cream"
 
 # favorite color
+print"blue and yellow"
